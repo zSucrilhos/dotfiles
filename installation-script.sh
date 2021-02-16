aba4126158abf87728c3ceb9eb05335bd174e96d
Erick Cesar <e.cesar88@gmail.com>
2021-02-15 23:38:16 -0500

    Or was it? (Added install script)

---
+++ installation-script.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Check bspwm rounded corners
+
+echo "### Installing..."
+echo "### Installing oh-my-zsh"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo "### First step done"
+echo "### Installing zplug"
+curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+zsh && zplug install
+
+echo "### Installing oh-my-tmux"
+cd $HOME
+git clone https://github.com/gpakosz/.tmux.git
+ln -s -f .tmux/.tmux.conf
+cp .tmux/.tmux.conf.local .
+
+# Copying files from dotfiles
+echo "### Cloning dotfiles"
+cd $HOME/Desktop
+git clone github.com/zsucrilhos/dotfiles && cd dotfiles
+
+echo "### Copying files..."
+cd extras && cp -rv ./_my-scripts ./icons ./themes .config $HOME && cd ..
+cd one_dark && cp -rv .config .tmux .Xresources .tmux.conf .tmux.conf.local .xinitrc $HOME && cd ..
+
+echo "### Installing yay" # maybe install paru later?
+git clone git clone https://aur.archlinux.org/yay.git 
+cd yay
+makepkg -si
+
+echo "### Installing packages..."
+cd ./dependencies/2021/
+sudo pacman -S --noconfirm - < ./pacman-packages.txt
+yay -S - < ./aur-packages.txt 
+
